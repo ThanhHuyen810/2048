535f1b3e36e467920731a7d975e7ae18c1bb233f
ThanhHuyen810 <37433694+ThanhHuyen810@users.noreply.github.com>
2018-05-11 13:56:40 +0700

    Update 2048.cpp

--- 2048.cpp
+++ 2048.cpp
@@ -124,7 +124,7 @@ void draw_background(){
 
 			if(i == 0 || i == 4 || i ==  8 || i == 12 || i == 16){
 				textcolor(136);
-				cout << 33;
+				cout << "  ";
 			}
 			else if(j == 0 || j == 4 || j ==  8 || j == 12 || j == 16){
 				textcolor(136);
